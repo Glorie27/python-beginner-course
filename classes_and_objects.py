7ae764a52057dcaf155300e3585b961caef29856
Ohenhen Isoken Glory <gloryohenhen5@gmail.com>
2026-03-09 20:58:21 +0100

    Add initial implementations for classes, inheritance, and student attributes

---
+++ classes_and_objects.py
@@ -0,0 +1,30 @@
+# class Myclass:
+#     x = 5
+
+# p1 = Myclass()
+# print(p1.x)
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))    
+# p1 = Person(name, age)
+# print(p1.name)
+# print(p1.age)
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+
+# p1 = Person("Glory", 73)
+# del p1
+# print(p1)
+
+
+class Person:
+    pass
